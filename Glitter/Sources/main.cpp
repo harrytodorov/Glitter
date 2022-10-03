62418064a208e9c89e552d600841d56d1aff83e7
Haralambi Todorov <haralambi.todorov@fifty2.eu>
2022-10-03 11:51:05 +0200

    LearnOpenGL, "Hello Triangle" chapter
    Draws two triangles next to each other
    using two separate vertex buffer objects
    and vertex array objects used within
    a single shader program.

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -8,41 +8,220 @@
 // Standard Headers
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
+
+// Shaders.
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+const char *fragmentShaderSourceL = "#version 330 core\n"
+                                    "out vec4 FragColor;\n"
+                                    "void main()\n"
+                                    "{\n"
+                                    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                    "}\0";
+const char *fragmentShaderSourceR = "#version 330 core\n"
+                                    "out vec4 FragColor;\n"
+                                    "void main()\n"
+                                    "{\n"
+                                    "   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+                                    "}\0";
 
 int main(int argc, char * argv[]) {
 
-    // Load GLFW and Create a Window
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
+  // Load GLFW and Create a Window
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+  auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
 
-    // Check for Valid Context
-    if (mWindow == nullptr) {
-        fprintf(stderr, "Failed to Create OpenGL Context");
-        return EXIT_FAILURE;
-    }
+  // Check for Valid Context
+  if (mWindow == nullptr) {
+    fprintf(stderr, "Failed to Create OpenGL Context");
+    return EXIT_FAILURE;
+  }
 
-    // Create Context and Load OpenGL Functions
-    glfwMakeContextCurrent(mWindow);
-    gladLoadGL();
-    fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+  // Create Context and Load OpenGL Functions
+  glfwMakeContextCurrent(mWindow);
+  gladLoadGL();
+  fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
 
-    // Rendering Loop
-    while (glfwWindowShouldClose(mWindow) == false) {
-        if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(mWindow, true);
+  // Build and compile shader program.
+  // ---
+  // Vertex shader.
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+  // Check compilation.
+  int  success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if(!success)
+  {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
 
-        // Background Fill Color
-        glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+  // Fragment shader left triangle.
+  unsigned int fragmentShaderLeftTriangle;
+  fragmentShaderLeftTriangle = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShaderLeftTriangle, 1, &fragmentShaderSourceL, NULL);
+  glCompileShader(fragmentShaderLeftTriangle);
+  // Check compilation.
+  glGetShaderiv(fragmentShaderLeftTriangle, GL_COMPILE_STATUS, &success);
+  if(!success)
+  {
+    glGetShaderInfoLog(fragmentShaderLeftTriangle, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::FRAGMENT::LEFT_TRIANGLE::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
 
-        // Flip Buffers and Draw
-        glfwSwapBuffers(mWindow);
-        glfwPollEvents();
-    }   glfwTerminate();
-    return EXIT_SUCCESS;
+  // Shader program left triangle.
+  unsigned int shaderProgramLeftTriangle;
+  shaderProgramLeftTriangle = glCreateProgram();
+  // Attach and link the shaders.
+  glAttachShader(shaderProgramLeftTriangle, vertexShader);
+  glAttachShader(shaderProgramLeftTriangle, fragmentShaderLeftTriangle);
+  glLinkProgram(shaderProgramLeftTriangle);
+  // Check linking.
+  glGetProgramiv(shaderProgramLeftTriangle, GL_LINK_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(shaderProgramLeftTriangle, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::PROGRAM::LEFT_TRIANGLE::LINKING_FAILED\n" << infoLog << std::endl;
+  }
+
+  // Fragment shader right triangle.
+  unsigned int fragmentShaderRightTriangle;
+  fragmentShaderRightTriangle = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShaderRightTriangle, 1, &fragmentShaderSourceR, NULL);
+  glCompileShader(fragmentShaderRightTriangle);
+  // Check compilation.
+  glGetShaderiv(fragmentShaderRightTriangle, GL_COMPILE_STATUS, &success);
+  if(!success)
+  {
+    glGetShaderInfoLog(fragmentShaderRightTriangle, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::FRAGMENT::RIGHT_TRIANGLE::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
+
+  // Shader program right triangle.
+  unsigned int shaderProgramRightTriangle;
+  shaderProgramRightTriangle = glCreateProgram();
+  // Attach and link the shaders.
+  glAttachShader(shaderProgramRightTriangle, vertexShader);
+  glAttachShader(shaderProgramRightTriangle, fragmentShaderRightTriangle);
+  glLinkProgram(shaderProgramRightTriangle);
+  // Check linking.
+  glGetProgramiv(shaderProgramRightTriangle, GL_LINK_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(shaderProgramRightTriangle, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::PROGRAM::RIGHT_TRIANGLE::LINKING_FAILED\n" << infoLog << std::endl;
+  }
+
+  // Delete the shaders.
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShaderLeftTriangle);
+  glDeleteShader(fragmentShaderRightTriangle);
+  // ---
+
+  // Set up vertex data and buffers, and configure vertex attributes.
+  // ---
+  float verticesOld[] = {
+      -0.5f, -0.5f, 0.0f, // left
+      0.5f, -0.5f, 0.0f, // right
+      0.0f,  0.5f, 0.0f  // top
+  };
+  float verticesLeftTriangle[] = {
+      -1.0f, -0.5f, 0.0f,  // bottom left
+      0.0f, -0.5f, 0.0f,  // bottom middle
+      -0.5f,  0.5f, 0.0f,  // top left
+  };
+  float verticesRightTriangle[] = {
+      0.0f, -0.5f, 0.0f,  // bottom middle
+      1.0f, -0.5f, 0.0f,  // bottom right
+      0.5f,  0.5f, 0.0f   // top right
+  };
+  //unsigned int indicesLeftTriangle[] =
+  //{
+  //    0, 1, 3,  // left triangle
+  //    1, 2, 4   // right triangle
+  //};
+
+  // Vertex buffer object and vertex array object.
+  unsigned int VBO[2], EBO, VAO[2];
+  glGenVertexArrays(2, VAO);
+  glGenBuffers(2, VBO);
+  //glGenBuffers(1, &EBO);
+  // We first bind the vertex array object for the left triangle.
+  glBindVertexArray(VAO[0]);
+
+  // Bind Vertex array buffer.
+  glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(verticesLeftTriangle), verticesLeftTriangle, GL_STATIC_DRAW);
+
+  // Bind element array buffer object.
+  //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  // Configure data layout of the current bound vertex array object.
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  // Bind the vertex array object for the right triangle.
+  glBindVertexArray(VAO[1]);
+
+  // Bind Vertex array buffer.
+  glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(verticesRightTriangle), verticesRightTriangle, GL_STATIC_DRAW);
+
+  // Configure data layout of the current bound vertex array object.
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  // uncomment this call to draw in wireframe polygons.
+  //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+  // Rendering Loop
+  while (glfwWindowShouldClose(mWindow) == false) {
+    if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+      glfwSetWindowShouldClose(mWindow, true);
+
+    // Background Fill Color
+    glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Draw left triangle!
+    glUseProgram(shaderProgramLeftTriangle);
+    glBindVertexArray(VAO[0]);
+    //glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glUseProgram(shaderProgramRightTriangle);
+    glBindVertexArray(VAO[1]);
+    // glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    //glBindVertexArray(0);
+    // glBindVertexArray(0);  // No need to unbind it every time, as we only
+    // have a single vertex array object.
+
+    // Flip Buffers and Draw
+    glfwSwapBuffers(mWindow);
+    glfwPollEvents();
+  }
+
+  // De-allocate all resources once they've outlived their purpose.
+  glDeleteVertexArrays(2, VAO);
+  glDeleteBuffers(2, VBO);
+  glDeleteProgram(shaderProgramLeftTriangle);
+  glDeleteProgram(shaderProgramRightTriangle);
+
+  glfwTerminate();
+  return EXIT_SUCCESS;
 }
